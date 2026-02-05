ad45ae4430a7ba015449f5a887cdffef5338c39e
GreekIgor <40692365+GreekIgor@users.noreply.github.com>
2026-02-05 15:11:38 +0500

    Update TestPackade.php

--- src/TestPackade.php
+++ src/TestPackade.php
@@ -1,10 +1,12 @@
 <?php
 namespace Src;
 class TestPackade{
+
+	private $conf;
 	
-    public function __construct(public array $conf = [])
+    public function __construct(array $conf = array())
     {
-        //
+       $this->conf = $conf;
     }
 
     public function Test()
